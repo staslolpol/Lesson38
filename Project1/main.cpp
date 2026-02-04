f984299b82dd657824bcee395784815ce24e681c
staslolpol <staspriho2010@gmail.com>
2026-02-04 17:14:14 +0300

    забыл защиту от дурака но добавил

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -13,6 +13,11 @@ int main() {
 	cout << "Input number of day: " << endl;
 	cin >> d;
 
+	if (n < 0 || d < 0) {
+		cout << "EROR 404";
+		return -1;
+	}
+
 	string exam = calculet_like(n, d);
 
 	cout << exam;
