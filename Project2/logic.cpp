339f1c17cd5a04a2a603b78028a81e97c5f1a79a
staslolpol <staspriho2010@gmail.com>
2026-02-04 17:26:48 +0300

    Задание 2

--- Project2/logic.cpp
+++ Project2/logic.cpp
@@ -1,1 +1,18 @@
 #include "logic.h"
+
+string search_sequence_of_odd_numbers(int n, int m) {
+	string result = "";
+
+	if (m < n) {
+		return "ERORR 404";
+	}
+
+	for (int i = 0; i < m; i++) {
+		if (i >= n && i % 2 == 1) {
+			result += to_string(i) + " ";
+		}
+
+
+	}
+	return result;
+}
--- Project2/logic.h
+++ Project2/logic.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+string search_sequence_of_odd_numbers(int n, int m);
